2571b0eeea599b099b23c850af632564c345c789
Yangyifan Wang <ywang20@mail.sfsu.edu>
2018-10-22 23:29:58 -0700

    main function and friend service
    update main fuction and add friend service

---
+++ src/main/java/FriendService.java
@@ -0,0 +1,39 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class FriendService {
+    private static MongoCollection<Document> friendColection;
+    public FriendService() {
+        // open connection
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        // get ref to database
+        MongoDatabase db = mongoClient.getDatabase("REST2");
+        // get ref to collection
+        friendColection = db.getCollection("friends");
+        friendColection.drop();
+        friendColection = db.getCollection("friends");
+    }
+
+    public String addFriend(String userID, String friendID) {
+        Document newFriendship = new Document().append("userID", userID).append("friendsID", friendID);
+        friendColection.insertOne(newFriendship);
+        return "okay";
+    }
+
+    public Set<String> getAllFriend(String userID) {
+        FindIterable<Document> allFriends = friendColection.find(eq("userID", userID));
+        Set<String> allFriendsID = new HashSet<>();
+        for (Document friend : allFriends) {
+            allFriendsID.add(friend.get("friendsID").toString());
+        }
+        return allFriendsID;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,100 @@
-package main.java;
+
+import org.bson.Document;
 import static spark.Spark.*;
 
+import java.util.Set;
+
+
 public class Main {
-
+    private static UserService userService = new UserService();
+    private static AuthService authService = new AuthService();
+    private static FriendService friendService = new FriendService();
+    
     public static void main(String[] args) {
-      // staticFiles.externalLocation("public");
-      // http://sparkjava.com/documentation
-      port(1234);
-      // calling get will make your app start listening for the GET path with the /hello endpoint
-      get("/hello", (req, res) -> "Hello World");
-
+        port(1236);
+        
+        get("/newuser", (req, res) -> {
+            Set<String> params = req.queryParams();
+            String username = null;
+            String password = null;
+            try {
+                username = req.queryParamsValues("username")[0];
+                password = req.queryParamsValues("password")[0];
+            } catch (Exception e) {
+                return "invalid input";
+            }
+            if (username == null || password == null || params.size() > 2) {
+                return "Invalid input";
+            }
+            String result = userService.createUser(username, password);
+            return result;
+        });
+        
+        get("login", (req, res) -> {
+            Set<String> params = req.queryParams();
+            String username = null;
+            String password = null;
+            try {
+                username = req.queryParamsValues("username")[0];
+                password = req.queryParamsValues("password")[0];
+            } catch (Exception e) {
+                return "invalid input";
+            }
+            if (username == null || password == null || params.size() > 2) {
+                return "invalid input";
+            }
+            String userID = userService.login(username, password);
+            if (userID.equals("login_failed")) {
+                return userID;
+            } else {
+                String token = authService.creatSession(userID);
+                return token;
+            }
+        });
+        
+        get("addfriend", (req, res) -> {
+            Set<String> params = req.queryParams();
+            String token = null;
+            String friend = null;
+            try {
+                token = req.queryParamsValues("token")[0];
+                friend = req.queryParamsValues("friend")[0];
+            } catch (Exception e) {
+                return "invalid input";
+            }
+            if (token == null || friend == null || params.size() > 2) {
+                return "invalid input";
+            }
+            Document userDoc = authService.findOwner(token);
+            if (userDoc == null) {
+                return "failed_authentication";
+            }
+            if (!userService.userExist(friend)) {
+                return "invalid input: friend id not found";
+            }
+            String userID = userDoc.get("_id").toString();
+            String result = friendService.addFriend(userID, friend);
+            return result;
+        });
+        
+        get("friends", (req, res) -> {
+            Set<String> params = req.queryParams();
+            String token = null;
+            try {
+                token = req.queryParamsValues("token")[0];
+            } catch (Exception e) {
+                return "invalid input";
+            }
+            if (token == null || params.size() > 1) {
+                return "Invalid input";
+            }
+            Document tokenOwner = authService.findOwner(token);
+            if (tokenOwner == null) {
+                return "failed_authentication";
+            }
+            String userID = tokenOwner.get("_id").toString();
+            Set<String> allFriends = friendService.getAllFriend(userID);
+            return allFriends;
+        });
     }
 }
